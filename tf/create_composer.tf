9982b304b81bf0c6ef94ec350cb832e5df9c97cb
HagaSpa <justicesparrow13@gmail.com>
2019-01-19 20:17:25 +0900

    composerの設定値変更。またpython_versionを指定するため、providerをbetaに変更

--- tf/create_composer.tf
+++ tf/create_composer.tf
@@ -1,14 +1,19 @@
 resource "google_composer_environment" "test-environment" {
+   provider = "google-beta"
    name = "sample"
    project = "studyproject-224907"
    region = "asia-northeast1"
    config {
-      node_count = 4
+      node_count = 3
 
       node_config {
          zone = "asia-northeast1-c"
          machine_type = "n1-standard-1"
-         disk_size_gb = 100
+         disk_size_gb = 20
+      }
+
+      software_config {
+         python_version = "3"
       }
    }
 }
